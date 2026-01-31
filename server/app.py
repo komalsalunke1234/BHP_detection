c6789878fdc924233a6c90dbeabf2f27d0391aba
Komal Salunke <komalsalunke1234@gmail.com>
2026-01-31 15:14:32 +0530

    done all

--- server/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-# ...existing code...
-app = Flask(__name__)
-CORS(app)
-# ...existing code...
-if __name__ == "__main__":
-    app.run(debug=True)
